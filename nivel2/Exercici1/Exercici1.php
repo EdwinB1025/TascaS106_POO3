fbf47cee49b7eb61d27d5057eeee0af2c05c08dd
Edwin <edwin.barrera1025@gmail.com>
2026-04-27 11:01:13 +0200

    modificacion ejercicio 1 nivel 2

--- nivel2/Exercici1/Exercici1.php
+++ nivel2/Exercici1/Exercici1.php
@@ -4,11 +4,15 @@ require_once 'ResourceType.php';
 require_once 'Topic.php';
 require 'DidacticResource.php';
 
-
 $a = new DidacticResource('PHP Avanzado', 'PkP', 'internet', 'https://itacademy.barcelonactiva.cat/mod/page/view.php?id=14508');
+echo $a->log;
 $a->setTopic('php');
+echo $a->log;
 $a->setResource('internet');
+echo $a->log;
 $a->setResource('web');
+echo $a->log;
 $b = new DidacticResource('PHP Avanzado', 'LARaVeL', 'Video', 'https://itacademy.barcelonactiva.cat/mod/page/view.php?id=14508');
+echo $a->log;
 echo var_dump($a);
 echo var_dump($b);
